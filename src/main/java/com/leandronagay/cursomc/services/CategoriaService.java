de68415e40b56759f375e5129c7369bc1c1e0613
leandronagay <leandro.nagay@gmail.com>
2020-09-10 15:42:31 +0200

    Listando todas categorias com DTO

--- src/main/java/com/leandronagay/cursomc/services/CategoriaService.java
+++ src/main/java/com/leandronagay/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.leandronagay.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,9 +39,12 @@ public class CategoriaService {
 		try {
 			repo.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
-		
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");	
 		}
-
 	}
+	
+	public List<Categoria> findAll() {
+		return repo.findAll();
+	}
+	
 }
