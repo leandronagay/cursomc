71dc247510b89812a043468db8e1fa1ba4bf9da0
leandronagay <leandro.nagay@gmail.com>
2020-09-10 16:49:58 +0200

    Validação sintática com Bean Validation

--- src/main/java/com/leandronagay/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/leandronagay/cursomc/dto/CategoriaDTO.java
@@ -2,12 +2,21 @@ package com.leandronagay.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.leandronagay.cursomc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+	
+	@NotEmpty(message="preenchimento obrigatório")
+	@Length (min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
+	
+	
 	private String nome;
 	
 	public CategoriaDTO() {
