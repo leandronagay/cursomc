c13e2f395ed06c2906b10766347f2fe8bcefc345
leandronagay <leandro.nagay@gmail.com>
2020-09-16 15:16:34 +0200

    Busca de produto por nome e categoria

--- src/main/java/com/leandronagay/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/leandronagay/cursomc/repositories/ProdutoRepository.java
@@ -1,11 +1,21 @@
 package com.leandronagay.cursomc.repositories;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.leandronagay.cursomc.domain.Categoria;
 import com.leandronagay.cursomc.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+	
+	@Query ("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat in :categorias")
+	Page<Produto> search (@Param("nome") String nome, List<Categoria> categorias, Pageable pageRequest);
 
 }
